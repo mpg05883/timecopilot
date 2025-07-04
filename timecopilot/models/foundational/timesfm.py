095fbcf7f6ade185df42fb336bcf38635183dc65
AzulGarza <fede.garza.ramirez@gmail.com>
2025-07-04 15:27:01 -0700

    fix: add timesfm checkpoint

--- timecopilot/models/foundational/timesfm.py
+++ timecopilot/models/foundational/timesfm.py
@@ -8,14 +8,31 @@ from ..utils.forecaster import Forecaster
 class TimesFM(Forecaster):
     def __init__(
         self,
-        repo_id: str = "google/timesfm-1.0-200m",
+        repo_id: str = "google/timesfm-1.0-200m-pytorch",
         context_length: int = 512,
-        batch_size: int = 64,
+        per_core_batch_size: int = 64,
+        num_layers: int = 20,
+        model_dims: int = 1280,
         alias: str = "TimesFM",
     ):
+        if "pytorch" not in repo_id:
+            raise ValueError(
+                "TimesFM only supports pytorch models, "
+                "if you'd like to use jax, please open an issue"
+            )
+
+        if "2.0" in repo_id:
+            raise ValueError(
+                "TimesFM 2.0 is not supported yet, "
+                "see https://github.com/google-research/timesfm/issues/269"
+                "please use TimesFM 1.0"
+            )
+
         self.repo_id = repo_id
         self.context_length = context_length
-        self.batch_size = batch_size
+        self.per_core_batch_size = per_core_batch_size
+        self.num_layers = num_layers
+        self.model_dims = model_dims
         self.alias = alias
 
     def get_predictor(
@@ -23,17 +40,19 @@ class TimesFM(Forecaster):
         prediction_length: int,
     ) -> timesfm.TimesFm:
         backend = "gpu" if torch.cuda.is_available() else "cpu"
-        tfm = timesfm.TimesFm(
-            context_len=self.context_length,
-            horizon_len=prediction_length,
-            input_patch_len=32,
-            output_patch_len=128,
-            num_layers=20,
-            model_dims=1280,
+        tfm_hparams = timesfm.TimesFmHparams(
             backend=backend,
-            per_core_batch_size=self.batch_size,
+            horizon_len=prediction_length,
+            context_len=self.context_length,
+            num_layers=self.num_layers,
+            model_dims=self.model_dims,
+            per_core_batch_size=self.per_core_batch_size,
         )
-        tfm.load_from_checkpoint(repo_id=self.repo_id)
+        tfm_checkpoint = timesfm.TimesFmCheckpoint(huggingface_repo_id=self.repo_id)
+        tfm = timesfm.TimesFm(
+            hparams=tfm_hparams,
+            checkpoint=tfm_checkpoint,
+        )
         return tfm
 
     def forecast(
