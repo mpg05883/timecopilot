7ec9508d30398c3ca39a85309f519e422f8beaf6
AzulGarza <fede.garza.ramirez@gmail.com>
2025-07-04 21:15:23 -0700

    tests: add chronos to main tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from timecopilot.agent import MODELS
+from timecopilot.models.foundational.chronos import Chronos
 
 benchmark_models = [
     "AutoARIMA",
@@ -8,3 +9,9 @@ benchmark_models = [
     "TimesFM",
 ]
 models = [MODELS[str_model] for str_model in benchmark_models]
+models.extend(
+    [
+        Chronos(repo_id="amazon/chronos-t5-tiny", alias="Chronos-T5"),
+        Chronos(repo_id="amazon/chronos-bolt-tiny", alias="Chronos-Bolt"),
+    ]
+)
