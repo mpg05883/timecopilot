1fff335fcae73d9cc73ea1187324ba9f082ec972
AzulGarza <fede.garza.ramirez@gmail.com>
2025-08-26 12:17:37 -0700

    fix: rm changelogs from testing paths

--- tests/docs/test_docs.py
+++ tests/docs/test_docs.py
@@ -3,13 +3,11 @@ from pathlib import Path
 import pytest
 from mktestdocs import check_md_file
 
+paths = [p for p in Path("docs").glob("**/*.md") if "changelogs" not in p.parts]
+
 
 @pytest.mark.docs
-@pytest.mark.parametrize(
-    "fpath",
-    Path("docs").glob("**/*.md"),
-    ids=str,
-)
+@pytest.mark.parametrize("fpath", paths, ids=str)
 def test_docs(fpath):
     check_md_file(fpath=fpath, memory=True)
 
